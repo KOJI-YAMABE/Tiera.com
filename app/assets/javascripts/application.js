de62b0362c8c203d389a8eef204679e0b5c93ff5
KOJI-YAMABE <kvbl.ak25@gmail.com>
2020-10-25 16:59:29 +0900

    Ajax thank comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,23 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require jquery
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
 //= require_tree .
+
+$(function() {
+ function readURL(input) {
+   if (input.files && input.files[0]) {
+     var reader = new FileReader();
+     reader.onload = function (e) {
+       $('#img_prev').attr('src', e.target.result);
+     }
+     reader.readAsDataURL(input.files[0]);
+   }
+ }
+ $("#image").change(function(){
+   readURL(this);
+ });
+});
--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -1,8 +1,20 @@
 class ThanksController < ApplicationController
 	before_action :authenticate_user!
-	def create
-	end
 
-	def destroy
-	end
+  def create
+    @post = Post.find(params[:post_id])
+    @post.thanks.create(user_id: current_user.id)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    thank = current_user.thanks.find_by(post_id: @post.id)
+    thank.destroy
+  end
+
+  private
+
+  def thank_params
+    params.require(:post).permit(:user_id, :post_id)
+  end
 end
