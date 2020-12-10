b27ab300c47dbf530edc49aa5c65d2e121d484d4
vagrant <vagrant@localhost.localdomain>
2020-12-10 14:38:30 +0000

    [edit]top.html.erb-layouts

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,24 @@
+(function($) {
+  "use strict";
+
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
+
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+})(jQuery); // End of use strict
