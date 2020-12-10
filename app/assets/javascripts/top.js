a0fce913a66a085e802f3120471074ccbbe436f1
vagrant <vagrant@localhost.localdomain>
2020-12-10 16:07:18 +0000

    [edit]top.html.erb-layouts

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,24 +1,18 @@
-(function($) {
-  "use strict";
+// (function($) {
+//   "use strict";
 
-  $(function(){
-    $('a[href^="#"]').click(function(){
-      var speed = 500;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
-      $("html, body").animate({scrollTop:position}, speed, "swing");
-      return false;
-    });
-  });
+//   $(function(){
+//     $('a[href^="#"]').click(function(){
+//       var speed = 500;
+//       var href= $(this).attr("href");
+//       var target = $(href == "#" || href == "" ? 'html' : href);
+//       var position = target.offset().top;
+//       $("html, body").animate({scrollTop:position}, speed, "swing");
+//       return false;
+//     });
+//   });
 
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
-  });
-
-})(jQuery); // End of use strict
+//   $('.js-scroll-trigger').click(function() {
+//     $('.navbar-collapse').collapse('hide');
+//   });
+// })(jQuery); // End of use strict
