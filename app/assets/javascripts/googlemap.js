198e244cf79e6402677a8da3291d34def64eaa9f
vagrant <vagrant@localhost.localdomain>
2020-12-13 14:26:03 +0000

    [edit]layout

--- app/assets/javascripts/googlemap.js
+++ app/assets/javascripts/googlemap.js
@@ -7,28 +7,27 @@ function initMap(){
   if(document.getElementById('map')){ //'map'というidを取得できたら実行
     map = new google.maps.Map(document.getElementById('map'), { //'map'というidを取得してマップを表示
       center: {lat: 35.6594666, lng: 139.7005536}, //初期値の設定
-      zoom: 15,
+      zoom: 5,
     });
   }else{ //'map'が無かった時
   console.log(gon.lat_lng);
     map = new google.maps.Map(document.getElementById('show_map'), { //'show_map'を取得してマップを表示
       center: {lat: 35.6594666, lng: 139.7005536},
-      zoom: 3,
+      zoom: 5,
     });
     var lat_lng ;
-    for(var i = 0; i <  gon.lat_lng.length; i++){
+    for(var i = 0; i < gon.lat_lng.length; i++){
       lat_lng = gon.lat_lng[i];
       console.log(lat_lng);
       console.log(lat_lng.latitude);
       var latlng = new google.maps.LatLng(parseFloat(lat_lng.latitude),parseFloat(lat_lng.longitude));
       console.log(latlng);
       var marker = new google.maps.Marker({ //GoogleMapにマーカーを落とす
-        position:  latlng, //マーカーを落とす位置を決める（値はDBに入っている）
+        position: latlng, //マーカーを落とす位置を決める（値はDBに入っている）
         map: map //マーカーを落とすマップを指定
       });
       console.log(marker);
       map.setCenter(latlng);
-
     }
   }
 }
